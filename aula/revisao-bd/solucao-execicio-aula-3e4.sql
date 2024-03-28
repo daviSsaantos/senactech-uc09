e476530c85205ebe484d49c95a3911cc41a5f380
cassio.trindade <cassiowt@gmail.com>
2024-03-28 09:31:47 -0300

    REsolução exercicio aula 3 a 4

--- aula/revisao-bd/solucao-execicio-aula-3e4.sql
+++ aula/revisao-bd/solucao-execicio-aula-3e4.sql
@@ -1,10 +1,19 @@
-	-- Qual o cliente que mais comprou produtos?
-	-- Qual o produto mais caro?
-	-- Quais produtos não foram vendidos?
-	-- Quais clientes não comprara, produtos?
-	-- Quais produtos foram comprados por quais clientes?
-	-- Qual é o total de venda de cada produto?
-	-- Qual é o total de comprado, em valor, por cada cliente?
+-- Qual o cliente que mais comprou produtos?
+-- Qual o produto mais caro?
+SELECT * FROM produtos ORDER BY preco DESC LIMIT 1;
 
-	-- Questão BONUS:
-	-- Calcule o estoque final de cada produto.
+SELECT *
+FROM produtos
+WHERE
+    preco = (
+        SELECT MAX(preco)
+        FROM produtos
+    );
+
+-- Quais produtos não foram vendidos?
+-- Quais clientes não comprara, produtos?
+-- Quais produtos foram comprados por quais clientes?
+-- Qual é o total de venda de cada produto?
+-- Qual é o total de comprado, em valor, por cada cliente?
+-- Questão BONUS:
+-- Calcule o estoque final de cada produto.
